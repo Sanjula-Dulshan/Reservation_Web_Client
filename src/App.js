71b37d58e6f110eb5a98eaf7af09ac598a1b7f47
Sanjula-Dulshan <sdulshan10@gmail.com>
2023-09-29 23:38:06 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import "/node_modules/bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <button className="btn btn-success">Button</button>{" "}
+    </div>
+  );
+}
