33099f54607aefdd86c826a5317975174d3dee33
Dulanjana9914 <it20265892@my.sliit.lk>
2023-10-07 21:19:34 +0530

    Train schedule component's frontend pages created

--- src/components/Train_Schedule/TrainScheduleUpdate.jsx
+++ src/components/Train_Schedule/TrainScheduleUpdate.jsx
@@ -1,16 +1,10 @@
-import { React, useState } from "react";
-import Modal from "react-modal";
-import trainGif from "./train2.gif";
-//import "./TrainSchedule.css";
-// import { useNavigate } from "react-router-dom";
-// import { useLocation } from "react-router-dom";
-import { BASE } from "../constants";
+import { React, useState, useEffect } from "react";
+import { BASE } from "../constants.js";
 
 export default function TrainSchedule() {
   const [stationRows, setStationRows] = useState([
     { stationName: "", arrivalTime: "" },
   ]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const addRow = () => {
     setStationRows([...stationRows, { stationName: "", arrivalTime: "" }]);
@@ -22,42 +16,16 @@ export default function TrainSchedule() {
     setStationRows(updatedRows);
   };
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+  //Get Train Schedule at the start
+  //   useEffect(() => {
+  //     getTrainSchedule();
+  //   }, []);
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  //Save Train Schedule
-  const saveTrainSchedule = async (e) => {
-    e.preventDefault();
-
-    // const response = await fetch(BASE + "/api/trainSchedule", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({
-    //     trainName: e.target.trainName.value,
-    //     seatCount: e.target.seatCount.value,
-    //     isActive: e.target.isActive.value,
-    //     stations: stationRows,
-    //   }),
-    // });
-    // const data = await response.json();
-
-    // Display the modal
-    openModal();
-
-    // After a delay, close the modal (you can adjust the delay)
-    setTimeout(() => {
-      closeModal();
-    }, 3000); // Close the modal after 3 seconds (adjust as needed)
-
-    //console.log(data);
-  };
+  //   const getTrainSchedule = async () => {
+  //     const response = await fetch(BASE + "/api/trainSchedule");
+  //     const data = await response.json();
+  //     console.log(data);
+  //   };
 
   return (
     <div className="container">
@@ -65,10 +33,10 @@ export default function TrainSchedule() {
         <div className="col-md-8">
           <div className="card mt-5">
             <div className="card-body">
-              <h1 className="text-center">Train Schedule</h1>
+              <h1 className="text-center">Update Train Schedule</h1>
 
-              {/* Add Train Schedule Form  */}
-              <form onSubmit={saveTrainSchedule}>
+              {/*Update Train Schedule Form  */}
+              <form>
                 <br></br>
                 <div className="mb-3">
                   <label htmlFor="trainName" className="form-label">
@@ -105,36 +73,13 @@ export default function TrainSchedule() {
                     <option value="false">Inactive</option>
                   </select>
                 </div>
-                {/* <div className="row">
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="stationName" className="form-label">
-                      Station Name
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="stationName"
-                      placeholder="Enter Station Name"
-                      required
-                    />
-                  </div>
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="arrivalTime" className="form-label">
-                      Arrival Time
-                    </label>
-                    <input
-                      type="time"
-                      className="form-control"
-                      id="arrivalTime"
-                      placeholder="Enter Arrival Time"
-                      required
-                    />
-                  </div>
-                </div> */}
                 {stationRows.map((row, index) => (
                   <div className="row mb-3" key={index}>
                     <div className="col-md-6 mb-3">
-                      <label htmlFor="stationName" className="form-label">
+                      <label
+                        htmlFor={`stationName-${index}`}
+                        className="form-label"
+                      >
                         Station Name
                       </label>
                       <input
@@ -151,7 +96,10 @@ export default function TrainSchedule() {
                       />
                     </div>
                     <div className="col-md-4 mb-3">
-                      <label htmlFor="arrivalTime" className="form-label">
+                      <label
+                        htmlFor={`arrivalTime-${index}`}
+                        className="form-label"
+                      >
                         Arrival Time
                       </label>
                       <input
@@ -167,17 +115,15 @@ export default function TrainSchedule() {
                         }}
                       />
                     </div>
-                    {index > 0 && (
-                      <div className="col-md-2 mb-3">
-                        <button
-                          type="button"
-                          className="btn btn-danger"
-                          onClick={() => removeRow(index)}
-                        >
-                          Close
-                        </button>
-                      </div>
-                    )}
+                    <div className="col-md-2 mb-3">
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        onClick={() => removeRow(index)}
+                      >
+                        Close
+                      </button>
+                    </div>
                   </div>
                 ))}
                 <button
@@ -190,25 +136,11 @@ export default function TrainSchedule() {
                 </button>
                 <br></br> <br></br>
                 <div className="text-center mt-2">
-                  <button type="submit" className="btn btn-success mt-2">
-                    Create Schedule
+                  <button type="submit" className="btn btn-success">
+                    Update Schedule
                   </button>
                 </div>
               </form>
-
-              {/* Modal for success */}
-              <Modal
-                isOpen={isModalOpen}
-                onRequestClose={closeModal}
-                className="success-modal"
-              >
-                <div className="modal-content">
-                  <h2>Success!</h2>
-                  <img src={trainGif} alt="Success GIF" />
-
-                  <p>Schedule Added Successfully.</p>
-                </div>
-              </Modal>
             </div>
           </div>
         </div>
