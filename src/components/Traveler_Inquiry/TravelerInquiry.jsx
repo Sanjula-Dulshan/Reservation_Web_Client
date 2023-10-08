ccfb82feb157cacec98e11206ecb65175daef3fd
Dilsha-thathsarani <dilsha99t@gmail.com>
2023-10-08 13:23:19 +0530

    Traveler profiles filter

--- src/components/Traveler_Inquiry/TravelerInquiry.jsx
+++ src/components/Traveler_Inquiry/TravelerInquiry.jsx
@@ -1,77 +1,53 @@
 import React, { useState, useEffect } from "react";
 import "./TravelerInquiry.css";
-import { FaCalendarAlt, FaPhone, FaInfoCircle } from "react-icons/fa"; // Import icons
+import { FaIdCard, FaEnvelope } from "react-icons/fa";
+import axios from "axios";
+import { BASE } from "../constants";
 
 const TravelerInquiries = () => {
-  const [inquiries, setInquiries] = useState([
-    {
-      id: 1,
-      name: "John Doe",
-      date: "2023-10-15",
-      description: "I have a question about my upcoming trip.",
-      telephone: "+1234567890",
-      pending: true,
-    },
-    {
-      id: 2,
-      name: "Jane Smith",
-      date: "2023-10-14",
-      description: "Can you help me with my reservation?",
-      telephone: "+9876543210",
-      pending: false,
-    },
-    {
-      id: 3,
-      name: "Alice Johnson",
-      date: "2023-10-13",
-      description: "I need assistance with my booking.",
-      telephone: "+1112223333",
-      pending: true,
-    },
-    // Add more dummy inquiries as needed
-  ]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    // In a real application, you would fetch traveler inquiries from an API or database here
-    // For this example, we initialize 'inquiries' with dummy data
+    axios
+      .get(`${BASE}/api/user`)
+      .then((response) => {
+        const filteredUsers = response.data.filter(
+          (user) => user.isTraveler && user.isAgent
+        );
+
+        setUsers(filteredUsers);
+        console.log("Filtered Users:", filteredUsers);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
   }, []);
 
   return (
     <div className="traveler-inquiries">
-      <h1 className="inquiries-title">Traveler Inquiries</h1>
+      <h1 className="inquiries-title">Traveler Profiles</h1>
       <div className="inquiry-cards">
-        {inquiries.map((inquiry) => (
-          <div className="inquiry-card" key={inquiry.id}>
+        {users.map((user, index) => (
+          <div className="inquiry-card" key={index}>
             <div className="inquiry-details">
-              <h3 className="inquiry-name">{inquiry.name}</h3>
+              <h3 className="inquiry-name">{user.name}</h3>
               <p className="inquiry-date">
                 <span className="icon">
-                  <FaCalendarAlt />
+                  <FaIdCard />
                 </span>
-                <strong>Date of Inquiry:</strong> {inquiry.date}
+                <strong>Nic:</strong> {user.nic}
               </p>
 
               <p className="inquiry-telephone">
                 <span className="icon">
-                  <FaPhone />
+                  <FaEnvelope />
                 </span>
-                <strong>Telephone:</strong> {inquiry.telephone}
-              </p>
-              <p className="inquiry-description">
-                <span className="icon">
-                  <FaInfoCircle />
-                </span>
-                <strong>Inquiry Description:</strong> {inquiry.description}
+                <strong>Email:</strong> {user.email}
               </p>
             </div>
-            <div className="inquiry-actions">
-              <button
-                className={`btn ${
-                  inquiry.pending ? "btn-pending" : "btn-resolved"
-                }`}
-              >
-                {inquiry.pending ? "Pending" : "Resolved"}
-              </button>
+            <div className="button-container">
+              <button className="update-button">Update</button>
+              <button className="delete-button">Delete</button>
             </div>
           </div>
         ))}
