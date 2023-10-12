9ce9d3b6a1fe41cd0762c4c22a712fadb5d30552
Dulanjana9914 <it20265892@my.sliit.lk>
2023-10-13 00:22:01 +0530

    logout functionality added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
   return (
     <header className="header">
       <i className="fas fa-train train-icon"></i>
@@ -10,6 +14,10 @@ const Header = () => {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
+        <a href="/" onClick={logout}>
+          {" "}
+          Logout
+        </a>
       </nav>
     </header>
   );
